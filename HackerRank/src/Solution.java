a89ca3c8439aef773ccbdee0e1c3dd6f6f3d6e8f
Vimal <Vimal@Dell>
2020-08-10 22:41:19 +0530

    First Commit

---
+++ HackerRank/src/Solution.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * @author Vimal
+ *
+ */
+public class Solution {
+	
+	public static void main (String args[]) {
+		Scanner scan = new Scanner(System.in);
+	    int a = scan.nextInt();
+	    System.out.println(a);
+	    
+	    // Complete this line
+
+	    int b = scan.nextInt();
+	    System.out.println(b);
+	    
+	    
+	    scan.close();
+	}
+}
